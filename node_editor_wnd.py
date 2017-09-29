3ffacd871e6af2331f372d6bc8f4a12e493f0ae1
Pavel Křupala <pavel.krupala@gmail.com>
2017-09-29 17:35:14 +0200

    tut02 - graphicsItems

--- node_editor_wnd.py
+++ node_editor_wnd.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 from node_graphics_scene import QDMGraphicsScene
+from node_graphics_view import QDMGraphicsView
 
 
 class NodeEditorWnd(QWidget):
@@ -21,12 +24,45 @@ class NodeEditorWnd(QWidget):
         self.grScene = QDMGraphicsScene()
 
         # create graphics view
-        self.view = QGraphicsView(self)
-        self.view.setScene(self.grScene)
+        self.view = QDMGraphicsView(self.grScene, self)
         self.layout.addWidget(self.view)
-
-
 
 
         self.setWindowTitle("Node Editor")
         self.show()
+
+
+        self.addDebugContent()
+
+
+
+    def addDebugContent(self):
+        greenBrush = QBrush(Qt.green)
+        outlinePen = QPen(Qt.black)
+        outlinePen.setWidth(2)
+
+
+        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
+
+        text = self.grScene.addText("This is my Awesome text!", QFont("Ubuntu"))
+        text.setFlag(QGraphicsItem.ItemIsSelectable)
+        text.setFlag(QGraphicsItem.ItemIsMovable)
+        text.setDefaultTextColor(QColor.fromRgbF(1.0, 1.0, 1.0))
+
+
+        widget1 = QPushButton("Hello World")
+        proxy1 = self.grScene.addWidget(widget1)
+        proxy1.setFlag(QGraphicsItem.ItemIsMovable)
+        proxy1.setPos(0, 30)
+
+
+        widget2 = QTextEdit()
+        proxy2 = self.grScene.addWidget(widget2)
+        proxy2.setFlag(QGraphicsItem.ItemIsSelectable)
+        proxy2.setPos(0, 60)
+
+
+        line = self.grScene.addLine(-200, -200, 400, -100, outlinePen)
+        line.setFlag(QGraphicsItem.ItemIsMovable)
+        line.setFlag(QGraphicsItem.ItemIsSelectable)
---
+++ node_graphics_view.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QGraphicsView
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class QDMGraphicsView(QGraphicsView):
+    def __init__(self, grScene, parent=None):
+        super().__init__(parent)
+        self.grScene = grScene
+
+        self.initUI()
+
+        self.setScene(self.grScene)
+
+
+    def initUI(self):
+        self.setRenderHints(QPainter.Antialiasing | QPainter.HighQualityAntialiasing | QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform)
+
+        self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
