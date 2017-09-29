08c67844ad1f6e8721d96469ff0cdd53293f7930
Pavel Křupala <pavel.krupala@gmail.com>
2017-09-29 18:26:08 +0200

    tut03 - mmb dragging

--- node_graphics_view.py
+++ node_graphics_view.py
@@ -20,3 +20,56 @@ class QDMGraphicsView(QGraphicsView):
 
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonPress(event)
+        elif event.button() == Qt.LeftButton:
+            self.rightMouseButtonPress(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonPress(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonRelease(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonRelease(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonRelease(event)
+        else:
+            super().mouseReleaseEvent(event)
+
+
+    def middleMouseButtonPress(self, event):
+        releaseEvent = QMouseEvent(QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
+                                   Qt.LeftButton, Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(releaseEvent)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers())
+        super().mousePressEvent(fakeEvent)
+
+
+
+    def middleMouseButtonRelease(self, event):
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() & ~Qt.LeftButton, event.modifiers())
+        super().mouseReleaseEvent(fakeEvent)
+        self.setDragMode(QGraphicsView.NoDrag)
+
+
+    def leftMouseButtonPress(self, event):
+        return super().mousePressEvent(event)
+
+    def leftMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event)
+
+    def rightMouseButtonPress(self, event):
+        return super().mousePressEvent(event)
+
+    def rightMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event)
+
