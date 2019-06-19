7ada676c3188cea2661d29f24d4ceb38f23ddc66
Pavel Krupala <pavel.krupala@gmail.com>
2019-06-19 12:39:49 +0200

    tut 42 - content for input and output sockets

--- examples/example_calculator/calc_conf_nodes.py
+++ examples/example_calculator/calc_conf_nodes.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import *
 from examples.example_calculator.calc_conf import *
 from examples.example_calculator.calc_node_base import *
 
@@ -8,6 +9,8 @@ class CalcNode_Add(CalcNode):
     op_code = OP_NODE_ADD
     op_title = "Add"
     content_label = "+"
+    content_label_objname = "calc_node_bg"
+
 
 @register_node(OP_NODE_SUB)
 class CalcNode_Sub(CalcNode):
@@ -15,6 +18,7 @@ class CalcNode_Sub(CalcNode):
     op_code = OP_NODE_SUB
     op_title = "Substract"
     content_label = "-"
+    content_label_objname = "calc_node_bg"
 
 @register_node(OP_NODE_MUL)
 class CalcNode_Mul(CalcNode):
@@ -22,6 +26,7 @@ class CalcNode_Mul(CalcNode):
     op_code = OP_NODE_MUL
     op_title = "Multiply"
     content_label = "*"
+    content_label_objname = "calc_node_mul"
 
 @register_node(OP_NODE_DIV)
 class CalcNode_Div(CalcNode):
@@ -29,24 +34,50 @@ class CalcNode_Div(CalcNode):
     op_code = OP_NODE_DIV
     op_title = "Divide"
     content_label = "/"
+    content_label_objname = "calc_node_div"
+
+
+class CalcInputContent(QDMNodeContentWidget):
+    def initUI(self):
+        self.edit = QLineEdit("1", self)
+        self.edit.setAlignment(Qt.AlignRight)
+        self.edit.setObjectName(self.node.content_label_objname)
+
 
 @register_node(OP_NODE_INPUT)
 class CalcNode_Input(CalcNode):
     icon = "icons/in.png"
     op_code = OP_NODE_INPUT
     op_title = "Input"
+    content_label_objname = "calc_node_input"
 
     def __init__(self, scene):
         super().__init__(scene, inputs=[], outputs=[3])
+
+    def initInnerClasses(self):
+        self.content = CalcInputContent(self)
+        self.grNode = CalcGraphicsNode(self)
+
+class CalcOutputContent(QDMNodeContentWidget):
+    def initUI(self):
+        self.lbl = QLabel("42", self)
+        self.lbl.setAlignment(Qt.AlignLeft)
+        self.lbl.setObjectName(self.node.content_label_objname)
 
 @register_node(OP_NODE_OUTPUT)
 class CalcNode_Output(CalcNode):
     icon = "icons/out.png"
     op_code = OP_NODE_OUTPUT
     op_title = "Output"
+    content_label_objname = "calc_node_output"
 
     def __init__(self, scene):
         super().__init__(scene, inputs=[1], outputs=[])
+
+    def initInnerClasses(self):
+        self.content = CalcOutputContent(self)
+        self.grNode = CalcGraphicsNode(self)
+
 
 
 # way how to register by function call
