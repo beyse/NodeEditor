2d6d0c8bff1f0ed36043efe10332bceb78ba3418
Pavel Krupala <pavel.krupala@gmail.com>
2020-04-24 16:15:52 +0200

    tut54 - increased version to v0.9.2

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/pavel.krupala/pyqt-node-editor-tutorials.git',
-    version='0.9.1',
+    version='0.9.2',
     zip_safe=False,
 )
