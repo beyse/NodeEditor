6f52a9c82a9d9dcf6f72fb13b9028d45eadb9891
Pavel Krupala <pavel.krupala@gmail.com>
2020-04-26 18:12:05 +0200

    increased version to be able to upload on PyPI

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/pavel.krupala/pyqt-node-editor-tutorials.git',
-    version='0.9.2',
+    version='0.9.21',
     zip_safe=False,
 )
