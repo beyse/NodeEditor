f4e6fab5573a86d363d36e5c389613d5219eddda
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-21 22:15:59 +0200

    Align node title in the center

--- nodeeditor/node_graphics_node.py
+++ nodeeditor/node_graphics_node.py
@@ -2,6 +2,7 @@
 """
 A module containing Graphics representation of :class:`~nodeeditor.node_node.Node`
 """
+from PyQt5.QtGui import QFontMetrics, QTextBlockFormat, QTextCursor
 from qtpy.QtWidgets import QGraphicsItem, QWidget, QGraphicsTextItem
 from qtpy.QtGui import QFont, QColor, QPen, QBrush, QPainterPath
 from qtpy.QtCore import Qt, QRectF
@@ -171,12 +172,12 @@ class QDMGraphicsNode(QGraphicsItem):
         self.title_item = QGraphicsTextItem(self)
         self.title_item.node = self.node
         self.title_item.setDefaultTextColor(self._title_color)
+        metrics = QFontMetrics(self._title_font)
+        width = metrics.width(self.node.title) 
         self.title_item.setFont(self._title_font)
-        self.title_item.setPos(self.title_horizontal_padding, 0)
-        self.title_item.setTextWidth(
-            self.width
-            - 2 * self.title_horizontal_padding
-        )
+        self.title_item.setX((self.width  - width)/ 2)
+        self.title_item.setY(0)
+        self.title_item.setTextWidth(self.width)
 
     def initContent(self):
         """Set up the `grContent` - ``QGraphicsProxyWidget`` to have a container for `Graphics Content`"""
