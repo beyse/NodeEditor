2e13a2c4821f35ed8c6388556facd31c830295c4
Sebastian Beyer <sebastian.beyer@live.com>
2021-11-22 00:41:03 +0100

    Set icon in windows taskbar

--- apps/execution_node_editor/main.py
+++ apps/execution_node_editor/main.py
@@ -1,6 +1,9 @@
 import os, sys
 from PyQt5 import QtCore, QtGui
 from qtpy.QtWidgets import QApplication
+import ctypes
+from sys import platform
+
 
 sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
 
@@ -22,6 +25,14 @@ if __name__ == '__main__':
     app_icon.addFile('gui/icons/128x128.png', QtCore.QSize(128,128))
     app_icon.addFile('gui/icons/256x256.png', QtCore.QSize(256,256))
     app.setWindowIcon(app_icon)
+
+
+    if platform == "win32":
+        # Windows...
+        #This will make sure that the app icon is set in the taskbar on windows
+        # See https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105 
+        myappid = u'no-company.node-editor.execution-graph-editor.1.0' # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
     wnd = ExecutionNodeEditorWindow()
     wnd.setWindowIcon(app_icon)
