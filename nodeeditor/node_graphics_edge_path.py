8bbb505f85732d6e0a938d32e23fe380754d218f
Sebastian Beyer <sebastian.beyer@live.com>
2021-12-01 00:21:06 +0100

    Fix weird math

--- nodeeditor/node_graphics_edge_path.py
+++ nodeeditor/node_graphics_edge_path.py
@@ -3,10 +3,6 @@ from qtpy.QtCore import QPointF
 from qtpy.QtGui import QPainterPath
 
 
-EDGE_CP_ROUNDNESS = 1     #: Bezier control point distance on the line
-WEIGHT_SOURCE = 0.2         #: factor for square edge to change the midpoint between start and end socket
-
-
 class GraphicsEdgePathBase:
     """Base Class for calculating the graphics path to draw for an graphics Edge"""
 
@@ -46,36 +42,17 @@ class GraphicsEdgePathBezier(GraphicsEdgePathBase):
         """
         s = self.owner.posSource
         d = self.owner.posDestination
-        dist = (d[0] - s[0]) * 0.5
+        dist = abs(d[0] - s[0]) * 0.5
 
-        cpx_s = +dist
-        cpx_d = -dist
-        cpy_s = 0
-        cpy_d = 0
-
-        if self.owner.edge.start_socket is not None:
-            ssin = self.owner.edge.start_socket.is_input
-            ssout = self.owner.edge.start_socket.is_output
-
-            if (s[0] > d[0] and ssout) or (s[0] < d[0] and ssin):
-                cpx_d *= -1
-                cpx_s *= -1
-
-                cpy_d = (
-                    (s[1] - d[1]) / math.fabs(
-                        (s[1] - d[1]) if (s[1] - d[1]) != 0 else 0.00001
-                    )
-                ) * EDGE_CP_ROUNDNESS
-                cpy_s = (
-                    (d[1] - s[1]) / math.fabs(
-                        (d[1] - s[1]) if (d[1] - s[1]) != 0 else 0.00001
-                    )
-                ) * EDGE_CP_ROUNDNESS
+        c1x = +dist
+        c2x = -dist
+        c1y = 0
+        c2y = 0
 
         path = QPainterPath(QPointF(self.owner.posSource[0], self.owner.posSource[1]))
         path.cubicTo( 
-            s[0] + cpx_s, s[1] + cpy_s, 
-            d[0] + cpx_d, d[1] + cpy_d, 
+            s[0] + c1x, s[1] + c1y, 
+            d[0] + c2x, d[1] + c2y, 
             self.owner.posDestination[0], 
             self.owner.posDestination[1])
         return path
