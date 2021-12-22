737e1443097245719573b974911fee14070083d6
Sebastian Beyer <sebastian.beyer@live.com>
2021-12-22 01:07:17 +0100

    Display syntax error in json editor

--- apps/execution_node_editor/json_editor.py
+++ apps/execution_node_editor/json_editor.py
@@ -1,6 +1,11 @@
+from json.decoder import JSONDecodeError
 import os
 from subprocess import call
 from PyQt5 import QtCore
+from PyQt5.QtGui import QBrush, QColor, QCursor
+from pyqode.core.api import syntax_highlighter
+from pyqode.core.api.decoration import TextDecoration
+import re
 
 from pyqode.qt import QtWidgets
 from pyqode.json.widgets import JSONCodeEdit
@@ -28,6 +33,7 @@ class JsonEditor(QtWidgets.QDockWidget):
         self.callback = None
 
     def update(self, json_dict, callback, active):
+        self.editor.decorations.clear()
         if not active:
             self.setWidget(None)
         else:
@@ -42,12 +48,26 @@ class JsonEditor(QtWidgets.QDockWidget):
         self.editor.setPlainText(self.original_content)
 
     def apply_change(self):
+        self.editor.decorations.clear()
+        self.setToolTip('')
+        self.setToolTipDuration(0)
         had_error = True
         json_dict = {}
         try:
             json_dict = json.loads(self.editor.toPlainText())
             had_error = False
-        except:
+        except JSONDecodeError as ex:
+            numbers = re.findall(r'\d+', str(ex))
+            line = int(numbers[0])
+            line -= 1
+            column = int(numbers[1])
+            char = int(numbers[2])
+            decoration = TextDecoration(self.editor.document(), char, char, line, line, 99, str(ex), True)
+            decoration.cursor.setPosition(char)            
+            decoration.set_as_error()
+            self.editor.decorations.append(decoration)
+            self.setToolTip(str(ex))
+            self.setToolTipDuration(25000)
             had_error = True
 
         if had_error == False and self.callback is not None:
