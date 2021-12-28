3ce67482c86323dbff90e7421500f17362232f85
Sebastian Beyer <sebastian.beyer@live.com>
2021-12-29 00:01:53 +0100

    Add zip packaging and comments

--- pack-ubuntu.sh
+++ pack-ubuntu.sh
@@ -1,3 +1,4 @@
+# create single binary from python source using PyInstaller
 python -m PyInstaller --noconfirm \
 --hidden-import PyQt5.QtWidgets \
 --hidden-import pyqode \
@@ -13,7 +14,12 @@ python -m PyInstaller --noconfirm \
 --name ExecutionNodeEditor \
 ./apps/execution_node_editor/main.py
 
+# make the file executable
 chmod 777 ./dist/ExecutionNodeEditor
 
+# copy assets and subsystem
 cp -a ./apps/execution_node_editor/execution_subsystem ./dist/execution_subsystem
 cp -a ./apps/execution_node_editor/assets ./dist/assets
+
+# zip the package
+cd dist && zip -r ExecutionNodeEditor_linux_x64.zip . && cd ..
